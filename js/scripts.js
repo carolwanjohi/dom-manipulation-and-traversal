1c77b1aed2887acbaf70d9d8614e110d0f49222a
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-08-10 11:57:19 +0300

    Created index.html and its css and js files to practice adding interactivity to web pages

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $("p#blue").click(function(){
+        $('p#blue').css('background-color', 'blue');
+    });
+    $("button#hello").click(function(){
+        $("ul#user").prepend("<li>Hello!</li>");
+        $("ul#webpage").prepend("<li>Why hello there!</li>");
+        $("ul#user").children('li').first().click(function(){
+            $(this).remove();
+        });
+        $("ul#webpage").children('li').first().click(function(){
+            $(this).remove();
+        });        
+    });
+
+    $("button#goodbye").click(function(){
+        $("ul#user").prepend("<li>Goodbye!</li>");
+        $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+    });
+
+    $("button#stop").click(function(){
+        $("ul#user").prepend("<li>Stop copying me!</li>");
+        $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+    });        
+});
